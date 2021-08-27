36e077e57cc171fae0eee97a58410c40cb8c7cd3
AngelOttoni <angel.m.ottoni@gmail.com>
2021-08-27 19:26:25 -0300

    implementação POST - prática 2

--- Node.JS/consumo-api-1/routes/compras.js
+++ Node.JS/consumo-api-1/routes/compras.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var cielo = require('../lib/cielo');
 
 /* POST criacao de compra. */
 router.post('/', function (req, res, next) {
-    res.send('Rodando...');
+    res.send(cielo.compra(req.body));
 });
 
 /* GET criacao de compra. */
